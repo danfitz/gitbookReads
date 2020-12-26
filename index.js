f25170a1ae29297a4b5c954c371396affd5b3fde
Dan Fitz <dan@danfitz.com>
2020-12-26 15:18:08 -0500

    Created basic script to convert SQLite bookmark data to markdown files for each book

---
+++ index.js
@@ -0,0 +1,71 @@
+const R = require('ramda');
+const db = require('better-sqlite3')('KoboReader.sqlite');
+const { writeFileSync } = require('fs');
+
+const rows = db
+  .prepare(
+    `
+      SELECT
+        B.Text as highlight,
+        B.Annotation as annotation,
+        B.DateCreated as dateCreated,
+        B.ChapterProgress as chapterProgress,
+        C.Title as title,
+        C.Attribution as author
+      FROM Bookmark as B
+      LEFT JOIN content as C ON B.VolumeID = C.ContentID
+      WHERE Text != ?
+      ORDER BY title, chapterProgress
+    `
+  )
+  .all('null');
+
+const groupByBooks = R.compose(
+  R.groupBy(R.prop('title')),
+  // TODO: Refactor this later
+  R.map(row => {
+    if (/\n[ \t]{2,}/g.test(row.highlight)) {
+      row.highlight = row.highlight.replace(/\n[ \t]{2,}/g, ' ').trim();
+    }
+    if (row.annotation) {
+      row.annotation = row.annotation.trim();
+    }
+    return row;
+  })
+);
+
+// TODO: Refactor this later
+const convertToMarkdown = R.map(book => {
+  const firstNote = book[0];
+  let markdown = `---
+title: ${firstNote.title}
+author: ${firstNote.author}
+---
+
+# *${firstNote.title}* by ${firstNote.author}`;
+
+  book.forEach((note, idx) => {
+    markdown += `\n\n**Location**: ${(note.chapterProgress * 100).toFixed(2)}%`;
+    markdown += `\n\n**Highlight**: ${note.highlight}`;
+    if (note.annotation) {
+      markdown += `\n\n**Annotation**: ${note.annotation}`;
+    }
+    if (idx !== book.length - 1) {
+      markdown += `\n\n---`;
+    }
+  });
+
+  return markdown;
+});
+
+const writeToFiles = R.compose(
+  // TODO: Refactor this later
+  R.map(([title, md]) =>
+    writeFileSync(`${title.split(' ').join('-').toLowerCase()}.md`, md)
+  ),
+  R.toPairs
+);
+
+const execute = R.compose(writeToFiles, convertToMarkdown, groupByBooks);
+
+execute(rows);
---
+++ infinite-jest.md
@@ -0,0 +1,18 @@
+---
+title: Infinite Jest
+author: David Foster Wallace
+---
+
+# *Infinite Jest* by David Foster Wallace
+
+**Location**: 16.62%
+
+**Highlight**: Opponents. It’s all educational. How promising you are as a Student of the Game
+
+---
+
+**Location**: 16.62%
+
+**Highlight**: Be a Student of the Game. Like most clichés of sport, this is profound. You can be shaped, or you can be broken. There is not much in between.
+
+**Annotation**: I love this idea! It's definitely a very nice thought to think about... But what about people who have no help?
