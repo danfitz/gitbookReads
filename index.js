620f811a94606ecd4b27876cd289cf8800e46f99
Dan Fitz <dan@danfitz.com>
2020-12-27 13:58:04 -0500

    Added error handling in case e-reader SQL file isn't found, added console log for file creation

--- index.js
+++ index.js
@@ -1,74 +1,86 @@
 const R = require('ramda');
-const db = require('better-sqlite3')(
-  '/Volumes/KOBOeReader/.kobo/KoboReader.sqlite'
-);
-const { writeFileSync } = require('fs');
 
-const rows = db
-  .prepare(
-    `
-      SELECT
-        B.Text as highlight,
-        B.Annotation as annotation,
-        B.DateCreated as dateCreated,
-        B.ChapterProgress as chapterProgress,
-        C.Title as title,
-        C.Attribution as author
-      FROM Bookmark as B
-      LEFT JOIN content as C ON B.VolumeID = C.ContentID
-      WHERE Text != ?
-      ORDER BY title, chapterProgress
-    `
-  )
-  .all('null');
+try {
+  const db = require('better-sqlite3')(
+    '/Volumes/KOBOeReader/.kobo/KoboReader.sqlite'
+  );
+  const { writeFileSync } = require('fs');
 
-const groupByBooks = R.compose(
-  R.groupBy(R.prop('title')),
+  const rows = db
+    .prepare(
+      `
+        SELECT
+          B.Text as highlight,
+          B.Annotation as annotation,
+          B.DateCreated as dateCreated,
+          B.ChapterProgress as chapterProgress,
+          C.Title as title,
+          C.Attribution as author
+        FROM Bookmark as B
+        LEFT JOIN content as C ON B.VolumeID = C.ContentID
+        WHERE Text != ?
+        ORDER BY title, chapterProgress
+      `
+    )
+    .all('null');
+
+  const groupByBooks = R.compose(
+    R.groupBy(R.prop('title')),
+    // TODO: Refactor this later
+    R.map(row => {
+      if (/\n[ \t]{2,}/g.test(row.highlight)) {
+        row.highlight = row.highlight.replace(/\n[ \t]{2,}/g, ' ').trim();
+      }
+      if (row.annotation) {
+        row.annotation = row.annotation.trim();
+      }
+      return row;
+    })
+  );
+
   // TODO: Refactor this later
-  R.map(row => {
-    if (/\n[ \t]{2,}/g.test(row.highlight)) {
-      row.highlight = row.highlight.replace(/\n[ \t]{2,}/g, ' ').trim();
-    }
-    if (row.annotation) {
-      row.annotation = row.annotation.trim();
-    }
-    return row;
-  })
-);
-
-// TODO: Refactor this later
-const convertToMarkdown = R.map(book => {
-  const firstNote = book[0];
-  let markdown = `---
+  const convertToMarkdown = R.map(book => {
+    const firstNote = book[0];
+    let markdown = `---
 title: ${firstNote.title}
 author: ${firstNote.author}
 ---
-
+  
 # *${firstNote.title}* by ${firstNote.author}`;
 
-  book.forEach((note, idx) => {
-    markdown += `\n\n### ${note.annotation ? 'Note' : 'Highlight'}
-
+    book.forEach((note, idx) => {
+      markdown += `\n\n### ${note.annotation ? 'Note' : 'Highlight'}
+  
 ${(note.chapterProgress * 100).toFixed(5)}%
-
+  
 > ${note.highlight.replace('\n', '\n> ')}`;
 
-    if (note.annotation) {
-      markdown += `\n\n**Comments**: ${note.annotation}`;
-    }
+      if (note.annotation) {
+        markdown += `\n\n**Comments**: ${note.annotation}`;
+      }
+    });
+
+    return markdown;
   });
 
-  return markdown;
-});
+  const mdFilename = title => `${title.split(' ').join('-').toLowerCase()}.md`;
 
-const writeToFiles = R.compose(
-  // TODO: Refactor this later
-  R.map(([title, md]) =>
-    writeFileSync(`${title.split(' ').join('-').toLowerCase()}.md`, md)
-  ),
-  R.toPairs
-);
+  const writeToFiles = R.compose(
+    // TODO: Refactor this later
+    R.map(
+      R.compose(
+        ([title, mdText]) => writeFileSync(mdFilename(title), mdText),
+        R.tap(([title]) => console.log(`Creating file: ${mdFilename(title)}`))
+      )
+    ),
+    R.toPairs
+  );
 
-const execute = R.compose(writeToFiles, convertToMarkdown, groupByBooks);
+  const execute = R.compose(writeToFiles, convertToMarkdown, groupByBooks);
 
-execute(rows);
+  execute(rows);
+} catch (error) {
+  console.log(
+    'Please ensure your Kobo e-reader is connected to your Mac. (This app assumes a Mac as well.)'
+  );
+}
--- infinite-jest.md
+++ infinite-jest.md
@@ -2,27 +2,27 @@
 title: Infinite Jest
 author: David Foster Wallace
 ---
-
+  
 # *Infinite Jest* by David Foster Wallace
 
 ### Note
-
+  
 13.74474%
-
+  
 > Good old traditional audio-only phone conversations allowed you to presume that the person on the other end was paying complete attention to you while also permitting you not to have to pay anything even close to complete attention to her.
 
 **Comments**: Lol this is a funny but probably very accurate theory about phone conversations.
 
 ### Highlight
-
+  
 13.88500%
-
+  
 > Good old aural telephone calls could be fielded without makeup, toupee, surgical prostheses, etc. Even without clothes, if that sort of thing rattled your saber. But for the image-conscious, there was of course no such answer-as-you-are informality about visual-video telephone calls,
 
 ### Note
-
+  
 14.16550%
-
+  
 > First there’s some sort of terrific, sci-fi-like advance in consumer tech — like from aural to video phoning — which advance always, however, has certain unforeseen disadvantages for the consumer; and then but the market-niches created by those disadvantages — like people’s stressfully vain repulsion at their own videophonic appearance — are ingeniously filled via sheer entrepreneurial verve; and yet the very advantages of these ingenious disadvantage-compensations seem all too often to undercut the original high-tech advance, resulting in consumer-recidivism and curve-closure
 
 **Comments**: I agree with the sentiment that technological advancement has a tendency to come along with negative side effects, which gives rise to new approaches to counteract those side effects. Does the net value become zero though? I'm more inclined to think the sum is net positive.
